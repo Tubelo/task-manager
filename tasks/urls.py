8c1e514ac5e88d867651917dd209d70747c1e4e1
Cristiano <cristubello98@gmail.com>
2026-05-14 10:09:08 -0300

    add delete and edit task - CRUD complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('nova/', views.task_create, name='task_create'),
+    path('editar/<int:pk>/', views.task_edit, name='task_edit'),
+    path('excluir/<int:pk>/', views.task_delete, name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
@@ -20,3 +20,23 @@ def task_create(request):
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form, 'title': 'Nova Tarefa'})
+
+@login_required
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form, 'title': 'Editar Tarefa'})
+
+@login_required
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk, owner=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
